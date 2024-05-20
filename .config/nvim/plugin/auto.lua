eae859c2bade260881da225e769ddf42a6192af3
John Liu <john-liu-einride@users.noreply.github.com>
2024-05-20 23:02:56 +0200

    feat(nvim): disable auto-comment in normal mode

--- .config/nvim/plugin/auto.lua
+++ .config/nvim/plugin/auto.lua
@@ -3,8 +3,15 @@
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+-- Disable auto-comment in normal mode
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    vim.opt.formatoptions:remove 'o'
   end,
 })
