0137b362cc6ec250384109c0edac9b582daf541f
John Liu <john-liu-einride@users.noreply.github.com>
2024-05-30 15:29:20 +0200

    feat(textobjects): add custom capture for function name

--- .config/nvim/lua/custom/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/custom/plugins/nvim-treesitter.lua
@@ -4,39 +4,41 @@ return { -- Highlight, edit, and navigate code
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
     'nvim-treesitter/playground',
+    'ghostbuster91/nvim-next',
   },
-  opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-    textobjects = {
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          [']f'] = { query = '@function.name', desc = 'Next function' },
-        },
-        goto_previous_start = {
-          ['[f'] = { query = '@function.name', desc = 'Prevous function' },
-        },
-      },
-    },
-    incremental_selection = {
-      enable = true,
-    },
-  },
-  config = function(_, opts)
+  config = function()
     -- Prefer git instead of curl in order to improve connectivity in some environments
     require('nvim-treesitter.install').prefer_git = true
     ---@diagnostic disable-next-line: missing-fields
-    require('nvim-treesitter.configs').setup(opts)
+    require('nvim-next.integrations').treesitter_textobjects()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+      nvim_next = {
+        enable = true,
+        textobjects = {
+          move = {
+            goto_next_start = {
+              [']f'] = { query = '@function.name', desc = 'Next function' },
+            },
+            goto_previous_start = {
+              ['[f'] = { query = '@function.name', desc = 'Prevous function' },
+            },
+          },
+        },
+      },
+      incremental_selection = {
+        enable = true,
+      },
+    }
   end,
 }
