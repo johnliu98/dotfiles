218950ded6aa25efda7548abe114a46af107e201
John Liu <john-liu-einride@users.noreply.github.com>
2024-07-08 08:36:32 +0200

    fix(nvim): more comfortable harpoon settings

--- .config/nvim/lua/custom/plugins/harpoon.lua
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -5,7 +5,7 @@ return {
     local mark = require 'harpoon.mark'
     local ui = require 'harpoon.ui'
 
-    vim.keymap.set('n', 'ma', function()
+    vim.keymap.set('n', 'mg', function()
       mark.add_file()
     end, { desc = 'Mark file' })
 
@@ -13,22 +13,20 @@ return {
       ui.toggle_quick_menu()
     end, { desc = 'Toggle menu' })
 
-    vim.keymap.set('n', 'm1', function()
+    vim.keymap.set('n', 'ma', function()
       ui.nav_file(1)
     end, { desc = 'Toggle menu' })
 
-    vim.keymap.set('n', 'm2', function()
+    vim.keymap.set('n', 'ms', function()
       ui.nav_file(2)
     end, { desc = 'Toggle menu' })
 
-    vim.keymap.set('n', 'm3', function()
+    vim.keymap.set('n', 'md', function()
       ui.nav_file(3)
     end, { desc = 'Toggle menu' })
 
-    vim.keymap.set('n', 'm4', function()
+    vim.keymap.set('n', 'mf', function()
       ui.nav_file(4)
     end, { desc = 'Toggle menu' })
   end,
 }
-
--- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
