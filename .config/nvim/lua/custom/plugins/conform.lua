4bc098fb0d5ea852748ebdf0187620a6b64648d5
John Liu <john-liu-einride@users.noreply.github.com>
2024-12-16 08:52:06 +0100

    fix(nvim/conform): disable auto formatting for proto files

--- .config/nvim/lua/custom/plugins/conform.lua
+++ .config/nvim/lua/custom/plugins/conform.lua
@@ -15,7 +15,7 @@ return { -- Autoformat
     require('conform').setup {
       notify_on_error = false,
       format_on_save = function(bufnr)
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, proto = true }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
