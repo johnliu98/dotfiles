5f203adc7e8e67c8f4bb9c06966cea4f1c61abcf
John Liu <john-liu-einride@users.noreply.github.com>
2025-03-15 18:16:35 +0100

    feat(nvim/diffview): add diffview plugin

---
+++ .config/nvim/lua/custom/plugins/diffview.lua
@@ -0,0 +1,7 @@
+return {
+  'sindrets/diffview.nvim',
+  config = function()
+    vim.keymap.set('n', '<leader>do', '<cmd>DiffviewOpen -uno<CR>', { desc = '[D]iffview [O]pen' })
+    vim.keymap.set('n', '<leader>dc', '<cmd>DiffviewClose<CR>', { desc = '[D]iffview [C]lose' })
+  end,
+}
