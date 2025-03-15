e3ebd9e3c0f1efcf726bc9cfe49c27cce69fa34e
John Liu <john-liu-einride@users.noreply.github.com>
2025-03-15 18:16:13 +0100

    fix(nvim/blink): prioritize LSP suggestions

--- .config/nvim/lua/custom/plugins/blink.lua
+++ .config/nvim/lua/custom/plugins/blink.lua
@@ -2,12 +2,39 @@ return {
   'saghen/blink.cmp',
   dependencies = 'rafamadriz/friendly-snippets',
   version = 'v0.*',
-  opts = {
-    keymap = { preset = 'default' },
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono',
-    },
-    signature = { enabled = true },
-  },
+  config = function()
+    require('blink.cmp').setup {
+      keymap = { preset = 'default' },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono',
+      },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
+        providers = {
+          lsp = {
+            score_offset = 1000,
+            fallbacks = { 'lazydev' },
+          },
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+          },
+        },
+      },
+      completion = {
+        menu = {
+          border = 'rounded',
+        },
+        documentation = { -- TODO: Add border to documentation.
+          auto_show = true,
+          auto_show_delay_ms = 500,
+          window = {
+            border = 'rounded',
+            max_width = 120,
+          },
+        },
+      },
+    }
+  end,
 }
