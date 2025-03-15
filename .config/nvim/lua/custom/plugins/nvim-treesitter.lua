8267c9887fe13ebac77885c774e4d625f84f96ba
John Liu <john-liu-einride@users.noreply.github.com>
2025-03-15 18:12:39 +0100

    chore(nvim/nvim-treesitter): remove diagnostic comment

--- .config/nvim/lua/custom/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/custom/plugins/nvim-treesitter.lua
@@ -9,7 +9,6 @@ return {
   config = function()
     -- Prefer git instead of curl in order to improve connectivity in some environments
     require('nvim-treesitter.install').prefer_git = true
-    ---@diagnostic disable-next-line: missing-fields
     require('nvim-next.integrations').treesitter_textobjects()
     require('nvim-treesitter.configs').setup {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
