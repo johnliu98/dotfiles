f04dffd611d1c81bafb14f1aab29a00bd6a4cc4c
John Liu <john-liu-einride@users.noreply.github.com>
2025-03-15 18:18:52 +0100

    chore(nvim/auto): remove auto-command for imports

--- .config/nvim/plugin/auto.lua
+++ .config/nvim/plugin/auto.lua
@@ -16,26 +16,6 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
--- Organize import on save
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.go',
-  callback = function()
-    local params = vim.lsp.util.make_range_params()
-    params.context = { only = { 'source.organizeImports' } }
-    local timeout_ms = 2000
-    local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params, timeout_ms)
-    for cid, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
-          vim.lsp.util.apply_workspace_edit(r.edit, enc)
-        end
-      end
-    end
-    vim.lsp.buf.format { async = false }
-  end,
-})
-
 -- Disable numbers of terminal
 -- Enter terminal in insert mode
 vim.api.nvim_create_autocmd('TermOpen', {
