e15da6c980de40120bac340727ae6c91f4ada830
John Liu <john-liu-einride@users.noreply.github.com>
2025-03-15 17:45:44 +0100

    chore(nvim/harpoon): update to harpoon 2

--- .config/nvim/lua/custom/plugins/harpoon.lua
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -1,38 +1,53 @@
 return {
   'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    require('harpoon').setup {
-      menu = {
-        width = 100,
+    local harpoon = require 'harpoon'
+
+    harpoon:setup {
+      settings = {
+        save_on_toggle = true,
       },
     }
 
-    local mark = require 'harpoon.mark'
-    local ui = require 'harpoon.ui'
-
     vim.keymap.set('n', 'mg', function()
-      mark.add_file()
-    end, { desc = 'Mark file' })
-
+      harpoon:list():add()
+    end, { desc = 'Add [M]ark' })
     vim.keymap.set('n', 'mt', function()
-      ui.toggle_quick_menu()
-    end, { desc = 'Toggle menu' })
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Toggle [M]ark UI' })
 
     vim.keymap.set('n', 'ma', function()
-      ui.nav_file(1)
-    end, { desc = 'Toggle menu' })
-
+      harpoon:list():select(1)
+    end, { desc = 'Goto [M]ark 1' })
     vim.keymap.set('n', 'ms', function()
-      ui.nav_file(2)
-    end, { desc = 'Toggle menu' })
-
+      harpoon:list():select(2)
+    end, { desc = 'Goto [M]ark 2' })
     vim.keymap.set('n', 'md', function()
-      ui.nav_file(3)
-    end, { desc = 'Toggle menu' })
-
+      harpoon:list():select(3)
+    end, { desc = 'Goto [M]ark 3' })
     vim.keymap.set('n', 'mf', function()
-      ui.nav_file(4)
-    end, { desc = 'Toggle menu' })
+      harpoon:list():select(4)
+    end, { desc = 'Goto [M]ark 4' })
+    vim.keymap.set('n', 'mr', function()
+      harpoon:list():select(5)
+    end, { desc = 'Goto [M]ark 4' })
+    vim.keymap.set('n', 'me', function()
+      harpoon:list():select(6)
+    end, { desc = 'Goto [M]ark 4' })
+    vim.keymap.set('n', 'mw', function()
+      harpoon:list():select(7)
+    end, { desc = 'Goto [M]ark 4' })
+    vim.keymap.set('n', 'mq', function()
+      harpoon:list():select(8)
+    end, { desc = 'Goto [M]ark 4' })
+
+    vim.keymap.set('n', 'mp', function()
+      harpoon:list():prev()
+    end, { desc = 'Goto Previous [M]ark' })
+    vim.keymap.set('n', 'mn', function()
+      harpoon:list():next()
+    end, { desc = 'Goto Next [M]ark' })
   end,
 }
