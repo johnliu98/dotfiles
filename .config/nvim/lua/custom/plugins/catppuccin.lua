bd66ebe5b0a90dd8dc91734d2a7477e0d16cd045
John Liu <john-liu-einride@users.noreply.github.com>
2025-03-15 17:52:52 +0100

    fix(nvim/catppuccin): use normal colors for floating buffers and dropdowns

--- .config/nvim/lua/custom/plugins/catppuccin.lua
+++ .config/nvim/lua/custom/plugins/catppuccin.lua
@@ -2,8 +2,29 @@ return {
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000,
-  init = function()
+  config = function()
+    require('catppuccin').setup {
+      flavour = 'mocha',
+      background = {
+        light = 'mocha',
+        dark = 'mocha',
+      },
+      integrations = {
+        blink_cmp = true,
+        harpoon = true,
+        notify = true,
+        mason = true,
+        noice = true,
+      },
+    }
+
     vim.cmd.colorscheme 'catppuccin'
     vim.cmd.hi 'Comment gui=none'
+
+    -- Prevents floating buffers to have a contrasting color outside the border.
+    vim.api.nvim_set_hl(0, 'NormalFloat', { link = 'Normal' })
+
+    -- Use same the colors for the auto-complete dropdown.
+    vim.api.nvim_set_hl(0, 'Pmenu', { link = 'Normal' })
   end,
 }
