696748a2445328f341c138856ae7adf857d35218
John Liu <john-liu-einride@users.noreply.github.com>
2025-03-15 17:55:19 +0100

    feat(nvim/floaterm): add borders to terminal

--- .config/nvim/lua/custom/plugins/floaterm.lua
+++ .config/nvim/lua/custom/plugins/floaterm.lua
@@ -3,10 +3,10 @@ return {
   keys = { '<C-t>' },
   cmd = { 'FloatermToggle' },
   config = function()
-    local g = vim.g
-    g.floaterm_width = 0.5
-    g.floaterm_height = 0.99
-    g.floaterm_position = 'right'
+    vim.g.floaterm_width = 0.5
+    vim.g.floaterm_height = 0.99
+    vim.g.floaterm_position = 'right'
+    vim.g.floaterm_borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' }
 
     vim.keymap.set({ 'n', 'v', 'x' }, '<C-t>', '<cmd>FloatermToggle<CR>', { desc = 'Floating [T]erminal' })
     vim.keymap.set('t', '<C-t>', '<C-\\><C-N><cmd>FloatermToggle<CR>', { desc = 'Floating [T]erminal' })
