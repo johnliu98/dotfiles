51b07ba61e605e311f044618bf751f9e69c0302f
John Liu <john-liu-einride@users.noreply.github.com>
2025-03-15 17:55:53 +0100

    feat(nvim/no-neck-pain): enabled plugin

--- .config/nvim/lua/custom/plugins/no-neck-pain.lua
+++ .config/nvim/lua/custom/plugins/no-neck-pain.lua
@@ -1,8 +1,8 @@
 return {
   'shortcuts/no-neck-pain.nvim',
-  enabled = false,
+  enabled = true,
   config = function()
-    require('no-neck-pain').setup { width = 130 }
+    require('no-neck-pain').setup { width = 140 }
     vim.keymap.set('n', '<C-s>', '<cmd>NoNeckPain<CR>', { desc = 'Center buffer' })
   end,
 }
