7158f0c13e7bd35a6fa73b55f30993256f7eabf1
John Liu <john-liu-einride@users.noreply.github.com>
2025-03-15 18:11:20 +0100

    feat(nvim/mini): show up to 1 million search results in statusline

--- .config/nvim/lua/custom/plugins/mini.lua
+++ .config/nvim/lua/custom/plugins/mini.lua
@@ -14,13 +14,34 @@ return { -- Collection of various small independent plugins/modules
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    -- require('mini.surround').setup {}
+
+    require('mini.pairs').setup {}
 
     local statusline = require 'mini.statusline'
-    statusline.setup { use_icons = vim.g.have_nerd_font }
 
-    statusline.section_location = function()
-      return '%2l:%-2v'
-    end
+    statusline.setup {
+      content = {
+        active = function()
+          local mode, mode_hl = statusline.section_mode {}
+          local git = statusline.section_git {}
+          local diagnostics = statusline.section_diagnostics {}
+          local filename = statusline.section_filename {}
+          local fileinfo = statusline.section_fileinfo {}
+          local search = statusline.section_searchcount { options = { recompute = true, maxcount = 1000000 } }
+          local location = '%2l/%2L:%-2v'
+          return statusline.combine_groups {
+            { hl = mode_hl, strings = { mode } },
+            { hl = 'MiniStatuslineDevinfo', strings = { git, diagnostics } },
+            '%<', -- Mark general truncate point
+            { hl = 'MiniStatuslineFilename', strings = { filename } },
+            '%=', -- End left alignment
+            { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+            { hl = mode_hl, strings = { search, location } },
+          }
+        end,
+      },
+      use_icons = vim.g.have_nerd_font,
+    }
   end,
 }
