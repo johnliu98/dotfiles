b28cc3c9badb7f4a515940cdc5ee3d2411d3b03e
John Liu <john-liu-einride@users.noreply.github.com>
2025-09-22 09:52:20 +0200

    feat(zsh): alias for git add patch

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -41,7 +41,8 @@ alias p="python"
 
 # --- Git Aliases ---
 alias ga="git add"
-alias gC="git commit"
+alias gap="git add --patch"
+alias gC="git commit -v"
 alias gCa="git commit --amend"
 alias gCan="git commit --amend --no-edit"
 alias gp="git pull -p"
@@ -49,7 +50,7 @@ alias gP="git push"
 alias gPf="git push --force-with-lease --force-if-includes"
 alias gc="git checkout"
 alias gcm="git checkout master"
-alias gs="git status"
+alias gs="git status --short"
 alias gl="git log --stat"
 alias glo="git log --oneline"
 alias glog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
@@ -70,9 +71,6 @@ function gbc() {
 done
 }
 
-
-# dependabot PRs
-alias db="gh dependabot -t einride/team-motion-control"
 
 # wireguard
 alias wg="sudo wg-quick up client_00044"
